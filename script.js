03f61ecc5e55728aafcf32a8c93fd813c5035035
Nikola Cerovina <cerovina1997@gmail.com>
2023-07-05 03:07:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var navLinks = document.querySelectorAll('nav ul li a');
+
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      var targetId = this.getAttribute('href');
+      var targetSection = document.querySelector(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
+
+
+
+window.chatbaseConfig = {
+  chatbotId: "IODFvRmvZiAalTC-nm2zJ",
+}
+
